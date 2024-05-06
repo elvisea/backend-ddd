f59fba693a1b2b7032bb3d5c3495c7a0371b7e2c
Elvis <eleram@protonmail.com>
2024-05-06 17:01:06 -0300

    Fórum com DDD & Clean Architecture - Factories de testes

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,11 +1,10 @@
-import dayjs from 'dayjs'
-
 import { Slug } from './value-objects/slug'
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
+import dayjs from 'dayjs'
 
-interface QuestionProps {
+export interface QuestionProps {
   authorId: UniqueEntityID
   bestAnswerId?: UniqueEntityID
   title: string
